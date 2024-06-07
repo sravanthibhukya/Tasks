dbff4b7fdf4befde0e909ad6bc0129a35eb36a28
sravanthibhukya <bhukyasravs@gmail.com>
2024-06-07 16:35:05 +0530

    Add files via upload

---
+++ admin-dashboard/src/main/java/com/example/demo/repository/Table2Repository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Table2;
+
+public interface Table2Repository extends JpaRepository<Table2, Long> {
+
+}
---
+++ admin-dashboard/src/main/java/com/example/demo/repository/Table3Repository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Table3;
+
+public interface Table3Repository extends JpaRepository<Table3, Long> {
+
+}
